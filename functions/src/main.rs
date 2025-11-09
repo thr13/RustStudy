8dbae6a648b1c61d5622a1e67ea63a5441af90d1
sugwan <skhrnt2945@gmail.com>
2025-11-09 21:59:34 +0900

    Docs: 함수 매개변수

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5, 'h');
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32, unit_label: char) {
+    println!("수치 값: {x}{unit_label}");
 }
