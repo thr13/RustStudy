c64ec0aed77f70d7cdc4756fb3aa5a26a0ec04bf
sugwan <skhrnt2945@gmail.com>
2025-11-10 11:47:43 +0900

    Docs: 구문과 표현식

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,19 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 'h');
+
+    expression();
 }
 
 fn another_function(x: i32, unit_label: char) {
     println!("수치 값: {x}{unit_label}");
+}
+
+fn expression() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("y 값: {y}");
 }
