bf2552f5ca4ecf2722b6b00700b7882ca74347b0
sugwan <skhrnt2945@gmail.com>
2025-11-10 11:54:07 +0900

    Docs: 반환값을 갖는 함수

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     another_function(5, 'h');
 
     expression();
+
+    let x = five();
+
+    println!("x 값: {x}")
 }
 
 fn another_function(x: i32, unit_label: char) {
@@ -17,4 +21,8 @@ fn expression() {
     };
 
     println!("y 값: {y}");
+}
+
+fn five() -> i32 {
+    5 // 세미콜론이 없으므로 값에 대한 표현식이다.
 }
