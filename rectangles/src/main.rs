d6a8959cd97abebc5197a3e1bb14c8997a9f6429
sugwan <skhrnt2945@gmail.com>
2025-11-11 23:56:44 +0900

    Feat: 연관 함수

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -113,6 +113,13 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool { // Rectangle 을 읽기만 하므로 불변 참조자 사용
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
