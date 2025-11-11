1e2019aa6b0c688f101310d4cafc0710ad44b21b
sugwan <skhrnt2945@gmail.com>
2025-11-11 17:26:36 +0900

    Feat: 구조체와 구조체 업데이트

---
+++ structs/src/main.rs
@@ -0,0 +1,51 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    /*
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+     */
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let email = String::from("test@example.com");
+    let username = String::from("test");
+    build_user(email, username);
+
+    let user2 = User {
+        email: String::from("another@example.com"), // 변경이 명시된 필드
+        ..user1
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+    /*
+    User {
+    active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+     */
+}
